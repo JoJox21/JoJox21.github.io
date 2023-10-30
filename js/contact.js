6cd68982368e5db8f24e7df37cf5134a6aebf307
John the beloved Dada <dj4real90@gmail.com>
2023-10-30 12:05:58 +0100

    Updated user details

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,7 @@
 const username = document.getElementById('username');
 const form = document.getElementById('submit');
 const password = document.getElementById('password');
+const errorMsg = document.getElementById('errormsg');
 
 // submit the form
 form.addEventListener('submit', e => {
@@ -31,31 +32,45 @@ const setError = (element, message) =>{
 }
 
  const validateInputs =() =>{
-    const usernameValue = username.value.trim();
-    const passwordValue = password.value.trim();
+    const usernameValue = username.value.trim().toLowerCase();
+    const passwordValue = password.value.trim().toLowerCase();
 
-    if(usernameValue === ''){ //check if usernameValue is empty
-        setError(username, 'Username is required');
-    }else{
-        setSuccess(username);
+    // if(usernameValue === ''){ //check if usernameValue is empty
+    //     setError(username, 'Username is required');
+    // }else{
+    //     setSuccess(username);
+    // }
+
+    // if(passwordValue === ''){ //check if passwordValue is empty
+    //     setError(password, 'Password is required');
+    // // }else if(passwordValue.length <8){ //check if password is less than 8 characters
+    // //     setError(password, 'Password must be at least 8 characters');
+    // }else{
+    //     setSuccess(password);
+    // }
+
+    if(usernameValue === "" || passwordValue === ""){
+        errorMsg.innerHTML = "Please enter your login credentials."
     }
 
-    if(passwordValue === ''){ //check if passwordValue is empty
-        setError(password, 'Password is required');
-    }else if(passwordValue.length <8){ //check if password is less than 8 characters
-        setError(password, 'Password must be at least 8 characters');
-    }else{
-        setSuccess(password);
+    else if((usernameValue === "ibineim" || usernameValue === "adikwund" || usernameValue === "hungevuaj" || usernameValue === "ohwojivwiogoroo" || usernameValue === "osadolore" || usernameValue === "dadanj") & passwordValue === "sterling123"){
+        window.location ='/pages/auction-page-all-items.html'
     }
+    else{
+        errorMsg.innerHTML = "These credentials do not match our records."
+        username.value = "";
+        password.value = "";
 
-    if(usernameValue && passwordValue){
-        if(passwordValue.length<8){
-            setError(password, 'Password must be at least 8 characters');
-        }
-        else{
-            window.location ='/pages/auction-page-all-items.html'
-        }
+        errorMsg.classList.add("shaking");
+
+        setTimeout(function () {
+            errorMsg.classList.remove("shaking");
+        }, 2000);
+
     }
+    
  }
 
-  
+// form.addEventListener('click', function(){
+
+// })
